86b3d85e145be05b20a806720fd458d92a803d54
Henry Chan <hechan@ucsd.edu>
2018-08-31 11:15:21 -0700

    added platform

--- static/js/player.js
+++ static/js/player.js
@@ -2,14 +2,23 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 450,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 300 },
+			debug: false
+		}
+	},
     scene: {
         preload: preload,
         create: create
+		update: update
     }
-}
+};
 
 var game = new Phaser.Game(config);
 var game_db = new Phaser.Data.DataManager(game);
+var platforms;
 
 function preload() {
     this.load.setBaseURL('https://avoidthebugs.herokuapp.com')
@@ -19,4 +28,8 @@ function preload() {
 
 function create() {
     this.add.image(400, 300, 'sky')
+	
+	platforms = this.physics.add.staticGroup();
+	
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 }
