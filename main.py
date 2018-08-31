2fb984f4aead848680d6e16f5f3d3f40b7c41bc6
Jeffrey Ha <jha@ucsd.edu>
2018-08-31 09:42:14 -0700

    hi

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from flask import Flask, url_for, render_template, request
+import pyrebase
+
+config = {
+    'apikey': 'AIzaSyBOuRNJdvdbb2x0qFWVHpZxbfiHS60EL5U',
+    'authDomain': 'avoidthebugs-b6e6e.firebaseapp.com',
+    'databaseUR'L: "https://avoidthebugs-b6e6e.firebaseio.com",
+    'projectId': "avoidthebugs-b6e6e",
+    'storageBucket': "avoidthebugs-b6e6e.appspot.com",
+    'messagingSenderId': "704938442068"
+}
 
 app = Flask(__name__)
 
@@ -12,4 +22,4 @@ def gamePage(): return render_template('game.html')
 def creditsPage(): return render_template('credits.html')
 
 if __name__ == '__main__':
-    app.run(port=5002,debug=False)
+    app.run(port=5003,debug=False)
--- static/js/player.js
+++ static/js/player.js
@@ -2,12 +2,6 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 450,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
     scene: {
         preload: preload,
         create: create
@@ -19,9 +13,10 @@ var game_db = new Phaser.Data.DataManager(game);
 
 function preload() {
     this.load.setBaseURL('https://avoidthebugs.herokuapp.com')
-    
+    this.load.image('sky', 'static/images/sky.png')
+
 }
 
 function create() {
-    
+    this.add.image(400, 300, 'sky')
 }
