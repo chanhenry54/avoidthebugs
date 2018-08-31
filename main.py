7db1dde09a5d6590cdc30fdc39aa5d7759404109
Jeffrey Ha <jha@ucsd.edu>
2018-08-30 20:50:00 -0700

    kale me now

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/'); # home page
-def mainPage():
-    return render_template('game.html')
+@app.route('/') # home page
+def mainPage(): return render_template('main.html')
+
+@app.route('/game') # game page
+def gamePage(): return render_template('game.html')
+
+@app.route('/credits') # credits page
+def creditsPage(): return render_template('credits.html')
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=false)
+    app.run(port=5000,debug=False)
